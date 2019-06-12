7b043877d5a4e32d10eb09c065b3c0eaa757ab26
Håvard Moås <haavamoa@gmail.com>
2019-06-12 12:02:58 +0200

    First commit :D

---
+++ src/FriendsApp/ViewModels/FriendViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FriendsApp.ViewModels
+{
+    public class FriendViewModel
+    {
+        public FriendViewModel(string name)
+        {
+            Name = name;
+            RemoveFriendCommand = new Command(RemoveFriend);
+        }
+        public string Name { get; }
+
+        public ICommand RemoveFriendCommand { get; }
+
+        public void RemoveFriend()
+        {
+            //How to communicate that we want to be deleted?
+            //Event? CallBack? Interface?
+        }
+
+    }
+}
---
+++ src/FriendsApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FriendsApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            AddFriendCommand = new Command(AddFriend);    
+            Friends = new ObservableCollection<FriendViewModel>();
+        }
+
+        public string NewFriendName { get; set; }
+        public ICommand AddFriendCommand { get; }
+
+        public void AddFriend()
+        {
+            Friends.Add(new FriendViewModel(NewFriendName));
+        }
+
+        public ObservableCollection<FriendViewModel> Friends { get; }
+    }
+}
